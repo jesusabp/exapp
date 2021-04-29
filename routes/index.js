f676a238064dda09d98f5e5e85a27d7bd355a94c
jesusabp <49287354+jesusabp@users.noreply.github.com>
2021-04-29 20:25:39 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -94,8 +94,8 @@ function insertOfertas(abbrev, ofertasNow){
 	  }
 	})
 
-	//client.query('SELECT * FROM ofertas', (err, res) => { // to test the connection.
-	client.query("INSERT INTO ofertas (abbrev, datetime, oferta) VALUES (\'"+abbrev+"\',now(),"+ofertasNow+");", (err, res) => {
+	client.query('SELECT * FROM ofertas', (err, res) => { // to test the connection.
+	//client.query("INSERT INTO ofertas (abbrev, datetime, oferta) VALUES (\'"+abbrev+"\',now(),"+ofertasNow+");", (err, res) => {
 			if (err) throw err;
 		for (let row of res.rows) {
 			console.log(JSON.stringify(row));
